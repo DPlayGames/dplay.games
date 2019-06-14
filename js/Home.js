9971329437187865c4e34881fbdaac60176acd94
Hanul <hanul@hanul.me>
2019-06-14 12:10:50 +0900

    로고 추가

--- js/Home.js
+++ js/Home.js
@@ -14,7 +14,14 @@ RUN(() => {
 				width : 300,
 				height : '100%',
 				backgroundColor : 'rgba(0, 0, 0, 0.4)'
-			}
+			},
+			c : [IMG({
+				style : {
+					marginTop : 50,
+					marginLeft : 40
+				},
+				src : 'resource/logo.png'
+			})]
 		}), CLEAR_BOTH()]
 	}).appendTo(BODY);
 });
